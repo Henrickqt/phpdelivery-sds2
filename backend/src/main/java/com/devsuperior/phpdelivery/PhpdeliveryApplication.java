6cecf1e19dca19b869f6a999197d511ade805c65
Henrickqt <pedro.msg@hotmail.com>
2021-01-10 17:32:53 -0200

    Project created

---
+++ backend/src/main/java/com/devsuperior/phpdelivery/PhpdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.phpdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhpdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhpdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/phpdelivery/PhpdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.phpdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhpdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
