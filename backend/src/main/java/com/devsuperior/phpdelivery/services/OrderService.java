2d8ab8fb6e6abd082d9a5682da5777613d480a92
Henrickqt <pedro.msg@hotmail.com>
2021-01-10 20:49:24 -0200

    PUT /orders/{id}/delivered

--- backend/src/main/java/com/devsuperior/phpdelivery/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/phpdelivery/services/OrderService.java
@@ -45,5 +45,15 @@ public class OrderService {
 
 		return new OrderDTO(order);
 	}
+	
+	@Transactional
+	public OrderDTO setAsDelivered(Long id) {
+		Order order = orderRepository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		
+		order = orderRepository.save(order);
+		
+		return new OrderDTO(order);
+	}
 
 }
