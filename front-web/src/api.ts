54a436c73601bdd097e30077c910eaf6e85efc16
Henrickqt <pedro.msg@hotmail.com>
2021-01-11 08:22:30 -0200

    Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,14 +1,15 @@
 import axios from "axios";
 import { OrderPayload } from './types/orderPayloadType';
 
-const API_URL = 'https://php-delivery.herokuapp.com';
+const API_URL = process.env.REACT_APP_API_URL_PROD;
+const TOKEN_MAPBOX = process.env.REACT_APP_ACCESS_TOKEN_MAPBOX;
 
 export function fetchProducts() {
   return axios(`${API_URL}/products`);
 }
 
 export function fetchLocalMapBox(local: string) {
-  return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${process.env.REACT_APP_ACCESS_TOKEN_MAPBOX}`);
+  return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${TOKEN_MAPBOX}`);
 }
 
 export function saveOrder(payload: OrderPayload) {
